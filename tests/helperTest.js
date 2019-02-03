709b21c1553e41bbca4aea037658a765281fa665
redaxmedia <info@redaxmedia.com>
2019-02-03 14:03:44 +0100

    More dynamic path walkter testing as order can be different

--- tests/helperTest.js
+++ tests/helperTest.js
@@ -10,12 +10,10 @@ describe('helper', () =>
 			.walkPath('./tests/provider/helper/*.html')
 			.then(contentArray =>
 			{
-				expect(contentArray[0].content).to.equal('1');
-				expect(contentArray[1].content).to.equal('2');
-				expect(contentArray[2].content).to.equal('3');
-				expect(contentArray[0].path).to.equal('./tests/provider/helper/1.html');
-				expect(contentArray[1].path).to.equal('./tests/provider/helper/2.html');
-				expect(contentArray[2].path).to.equal('./tests/provider/helper/3.html');
+				contentArray.forEach(contentValue =>
+				{
+					expect(contentValue.path).to.equal('./tests/provider/helper/' + contentValue.content + '.html');
+				});
 				done();
 			})
 			.catch(() => null);
