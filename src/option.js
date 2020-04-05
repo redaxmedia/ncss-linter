1ef3d207282cd98a19d64ac06a9f5787efa77d34
redaxmedia <info@redaxmedia.com>
2020-04-05 03:49:02 +0200

    Using __dirname for option.json

--- src/option.js
+++ src/option.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const option = require('utility-redaxmedia').option('option.json');
+const option = require('utility-redaxmedia').option(__dirname + '/../option.json');
 
 let configArray = {};
 
